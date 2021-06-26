6f007ee9ff9b03d8b00e4f83eeb6331462a9452c
Tri Phan <phanphutri@gmail.com>
2021-06-26 17:25:16 +0700

    Started chapter 8

---
+++ Part_03_Digging_into_objects/Chapter_08_Controlling_access_to_objetcs/8_1_Controlling_access_to_properties_with_getters_and_setters.js
@@ -0,0 +1,9 @@
+/**
+ * In the previous chapter, you saw that Javascript objects are dynamically collections of properties.
+ * We can easily add new properties, change the value of properties, and even completely remove
+ * existing properties. In many situations (for example, when validating property values, logging, or
+ * displaying data in the UI), we need to be able to monitor exactly what's going on with our objects.
+ *
+ * So in this chapter, you'll learn techniques for controlling access to and monitoring all of the changes
+ * that occur in your objects.
+ */
